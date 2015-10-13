5b15056a7fca02b7885de97f879361d668c6cef9
Evan Smith <esmith49@cnm.edu>
2015-10-13 13:11:40 -0600

    Added .gitigrone for Deep Dive Projects testing paragraph breaks

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 		<title>The Unpredictable Life of Evan</title>
 	</head>
 	<body>
-		<h1>Corporations are EVIL</h1>
+		<h1>The Unpredictable Life of Evan</h1>
+		<h2>Corporations are EVIL</h2>
 		<p>I loved working in the travel industry.  I was selling happiness and getting to travel the world to boot, then the internet came along and changed everything.  The small to medium sized travel agencies and wholesale brokers were dying and didn't even know it yet.  I was recruited by some small tech companies for my knowledge of the travel industry and my access to people that grant Wholesale and Consolidator contracts for airlines, hotels, cruise ships and car rental companies.  The team that I had put together from among my previous co workers got to work building databases and getting contracts.  The job was AMAZING at first, the bosses would come to me and say "Who wants to go to Maui for the weekend and negoiate with the 4 seasons?".	Then the dark days hit, we had recieved most of the airline and cruise ship contracts so the conversations started to turn to "Who wants to go to Cleveland on a Tuesday in February and stay at Motel 6?  I lost my job soon after and finally figured out that I had done such a good job that they didn't need me anymore.</p>
 	</body>
 </html>
